8b57bf9631ecb3df9b2736afbacbc13a5244f7e7
jacek010 <jmyjkowski@gmail.com>
2023-11-22 19:36:15 +0100

    first try of dockering

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
    
-engine = create_engine('mysql+pymysql://root:root_password@localhost:3306/budget_db')
+engine = create_engine('mysql+pymysql://root:root_password@db:3306/budget_db')
 session = Session(engine)
 
 def get_db():
